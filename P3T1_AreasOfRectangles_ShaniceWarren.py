804b87849f477fa953965e9f7186a322a7504aa3
shanicewarren24 <55643608+shanicewarren24@users.noreply.github.com>
2019-09-29 17:39:30 -0400

    Add files via upload
    P3T1- Areas of Rectangles

---
+++ P3T1_AreasOfRectangles_ShaniceWarren.py
@@ -0,0 +1,21 @@
+# CTI-110
+# Area of Rectangle
+# Shance Warren
+
+
+Length1 = float(input('Enter the length of rectangle 1 '))
+
+Width1= float(input('Enter the width of rectangle 1 '))
+
+Length2 = float(input('Enter the length of rectangle 2 '))
+
+Width2 = float(input('Enter the width of rectangle 2 '))
+
+Area1 = Length1 * Width1
+
+Area2 = Length2 * Width2
+
+if Area1 > Area2: print('The area of rectangle 1 is bigger.')
+if Area2 > Area1: print('The area of rectangle 2 is bigger.')
+if Area1 == Area2: print('The area of both rectangles are same.')                         
+ 
